274557efa91d477bb7244187b77f48c27d944cf5
Hanyu Wu <hy@Hanyus-MacBook-Air.local>
2022-10-09 00:04:07 +0800

    created sample etl flow with prefect

---
+++ src/etl_example.py
@@ -0,0 +1,43 @@
+import json
+import requests
+import pandas as pd
+from datetime import datetime
+from prefect import task, flow
+
+@task
+def extract(url: str) -> dict:
+    res = requests.get(url)
+    if not res:
+        raise Exception('No data fetched!')
+    return json.loads(res.content)
+
+@task
+def transform(data: dict) -> pd.DataFrame:
+    transformed = []
+    for user in data:
+        transformed.append({
+            'ID': user['id'],
+            'Name': user['name'],
+            'Username': user['username'],
+            'Email': user['email'],
+            'Address': f"{user['address']['street']}, {user['address']['suite']}, {user['address']['city']}",
+            'PhoneNumber': user['phone'],
+            'Company': user['company']['name']
+        })
+    return pd.DataFrame(transformed)
+
+@task
+def load(data: pd.DataFrame, path: str) -> None:
+    data.to_csv(path_or_buf=path, index=False)
+    
+
+@flow(name='simple_etl_pipeline')
+def prefect_flow():
+    users = extract(url='https://jsonplaceholder.typicode.com/users')
+    df_users = transform(users)
+    load(data=df_users, path=f'../data/users_{int(datetime.now().timestamp())}.csv')
+
+
+if __name__ == '__main__':
+    prefect_flow()
+    
