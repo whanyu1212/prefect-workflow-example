20c37de023b7d7854cf2744c966a687ca46d07c6
whanyu1212 <107110503+whanyu1212@users.noreply.github.com>
2022-10-10 09:47:54 +0000

    added unit tests

--- tests/test_etl_example.py
+++ tests/test_etl_example.py
@@ -3,6 +3,7 @@ import os.path
 import pandas as pd
 import json
 import requests
+import pytest
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from src.etl_example import (
@@ -11,9 +12,14 @@ from src.etl_example import (
     load,
     prefect_flow
 )
+from datetime import datetime
+from pathlib import Path
 from typing import Dict, List
 from collections.abc import Iterable
-import pytest
+from prefect import task, flow
+from prefect.testing.utilities import prefect_test_harness
+from pathlib import Path
+
 
 
 testurl = ['https://jsonplaceholder.typicode.com/comments']
@@ -28,30 +34,45 @@ def test_extract(testurl: str):
     for i in json.loads(res.content):
         assert isinstance(i,dict)
 
-# @pytest.fixture
-# def example_data():
-#     return [
-#             {
-#                 "id": 1,
-#                 "name": "Leanne Graham",
-#                 "username": "Bret",
-#                 "email": "Sincere@april.biz",
-#                 "address": {
-#                 "street": "Kulas Light",
-#                 "suite": "Apt. 556",
-#                 "city": "Gwenborough",
-#                 "zipcode": "92998-3874",
-#                 "geo": {
-#                     "lat": "-37.3159",
-#                     "lng": "81.1496"
-#                 }
-#                 },
-#                 "phone": "1-770-736-8031 x56442",
-#                 "website": "hildegard.org",
-#                 "company": {
-#                 "name": "Romaguera-Crona",
-#                 "catchPhrase": "Multi-layered client-server neural-net",
-#                 "bs": "harness real-time e-markets"
-#                 }
-#             }
-#             ]
+@pytest.fixture
+def example_data():
+    return [
+            {
+                "id": 1,
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "address": {
+                "street": "Kulas Light",
+                "suite": "Apt. 556",
+                "city": "Gwenborough",
+                "zipcode": "92998-3874",
+                "geo": {
+                    "lat": "-37.3159",
+                    "lng": "81.1496"
+                }
+                },
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "company": {
+                "name": "Romaguera-Crona",
+                "catchPhrase": "Multi-layered client-server neural-net",
+                "bs": "harness real-time e-markets"
+                }
+            }
+            ]
+
+
+def test_transform(example_data):
+    assert len(transform.fn(example_data)) !=0
+
+
+@pytest.fixture(autouse=True, scope="session")
+def prefect_test_fixture():
+    with prefect_test_harness():
+        yield
+
+def test_prefect_flow():
+    prefect_flow()
+    path = Path('../data/users_'+str(int(datetime.now().timestamp()))+'.csv')
+    assert path.is_file()
