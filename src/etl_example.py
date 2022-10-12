8973e1d9015699a61f194a76a65922af8858ce2f
whanyu1212 <107110503+whanyu1212@users.noreply.github.com>
2022-10-12 08:39:03 +0000

    added pre-commit hooks

--- src/etl_example.py
+++ src/etl_example.py
@@ -1,44 +1,79 @@
 import json
-import requests
-import pandas as pd
 from datetime import datetime
-from prefect import task, flow
+
+import pandas as pd
+import requests
+from prefect import flow, task
 
 
 @task
 def extract(url: str) -> dict:
+    """extract data from url
+
+    Args:
+        url (str): url in string
+
+    Raises:
+        Exception: unless nothing is fetched
+
+    Returns:
+        dict: data stored in dictionary format
+    """
     res = requests.get(url)
     if not res:
-        raise Exception('No data fetched!')
+        raise Exception("No data fetched!")
     return json.loads(res.content)
+
 
 @task
 def transform(data: dict) -> pd.DataFrame:
+    """Transform dictionary to dataframe
+
+    Args:
+        data (dict): fetched from url
+
+    Returns:
+        pd.DataFrame: transformed dataframe
+    """
     transformed = []
     for user in data:
-        transformed.append({
-            'ID': user['id'],
-            'Name': user['name'],
-            'Username': user['username'],
-            'Email': user['email'],
-            'Address': f"{user['address']['street']}, {user['address']['suite']}, {user['address']['city']}",
-            'PhoneNumber': user['phone'],
-            'Company': user['company']['name']
-        })
+        transformed.append(
+            {
+                "ID": user["id"],
+                "Name": user["name"],
+                "Username": user["username"],
+                "Email": user["email"],
+                "Address": f"{user['address']['street']}, \
+                    {user['address']['suite']}, \
+                        {user['address']['city']}",
+                "PhoneNumber": user["phone"],
+                "Company": user["company"]["name"],
+            }
+        )
     return pd.DataFrame(transformed)
+
 
 @task
 def load(data: pd.DataFrame, path: str) -> None:
+    """Store transformed data in csv format
+
+    Args:
+        data (pd.DataFrame): from previous step
+        path (str): designated folder ../data
+    """
     data.to_csv(path_or_buf=path, index=False)
-    
 
-@flow(name='simple_etl_pipeline')
+
+@flow(name="simple_etl_pipeline")
 def prefect_flow():
-    users = extract(url='https://jsonplaceholder.typicode.com/users')
+    """orchestrate the entire flow"""
+    users = extract(url="https://jsonplaceholder.typicode.com/users")
     df_users = transform(users)
-    load(data=df_users, path='../data/users_'+str(int(datetime.now().timestamp()))+'.csv')
+    load(
+        data=df_users,
+        path="../data/users_" + str(int(datetime.now().timestamp())) + ".csv",
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     prefect_flow()
-    
